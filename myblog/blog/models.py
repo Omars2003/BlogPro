4f6b8124f03c6bc6b64117abaa80c31844ec8664
Omar <omartinezgarcia670@gmail.com>
2024-08-23 05:53:57 +0000

    95% del 2

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,19 +1,16 @@
-from django.db import models
-from django.utils import timezone
 from django.conf import settings
+from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
-#Custom manager
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return(
+        return (
             super().get_queryset().filter(status=Post.Status.PUBLISHED)
         )
 
 
-
-
-# Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -29,11 +26,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
-
-
-
-
-    body  = models.TextField()
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -42,9 +35,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
-    objects =models.Manager() #default manager
-    published = PublishedManager()  #costum manager
 
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
@@ -55,9 +48,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-        def get_absolute_url(self):
-
-           return reverse(
+    def get_absolute_url(self):
+        return reverse(
             'blog:post_detail',
             args=[
                 self.publish.year,
@@ -66,4 +58,30 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+
+        
+
+        
 
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -39,18 +34,34 @@ def post_detail(request, year, month, day, post):
         Post,
         status=Post.Status.PUBLISHED,
         slug=post,
-        publish_year=year,
-        publish_month=month,
-        publish_day=day
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
     )
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+     {
+        'post': post,
+        'comments': comments,
+        'form':form
+    }
     )
+  
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_share(request, post_id):
+    # Retrieve post by id
     post = get_object_or_404(
         Post,
         id=post_id,
@@ -59,28 +70,30 @@ def post_share(request, post_id):
     sent = False
 
     if request.method == 'POST':
-
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            cd =form.cleaned_data
+            # Form fields passed validation
+            cd = form.cleaned_data
             post_url = request.build_absolute_uri(
                 post.get_absolute_url()
             )
             subject = (
-                f"{cd['name']}({cd['email']})"
-                f"recommends you read{post.title}"
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
             )
             message = (
-                f"Read {post.title} at  {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
                 subject=subject,
                 message=message,
                 from_email=None,
-                recipient_list=[cd['to']]
+                recipient_list=[cd['to']],
             )
-            sent=True
+            sent = True
+    
 
     else:
         form = EmailPostForm()
@@ -88,8 +101,31 @@ def post_share(request, post_id):
         request,
         'blog/post/share.html',
         {
-            'post':post,
-            'form':form,
-            'sent':sent
+            'post': post,
+            'form': form,
+            'sent': sent
+        },)
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form  = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment':comment
         }
     )
