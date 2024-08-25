e8fd328defa0fb64875ec28a35bf4d865e151d65
Omar <omartinezgarcia670@gmail.com>
2024-08-25 05:27:16 +0000

    tag

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -12,6 +12,7 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
+    tags = TaggableManager()
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
