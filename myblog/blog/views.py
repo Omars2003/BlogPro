6e2603134679ba3e0fa659a6a6d28b85554a7021
Omar <omartinezgarcia670@gmail.com>
2024-08-26 02:09:55 +0000

    tags con busqueda

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -5,10 +5,17 @@ from .models import Post
 from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -25,7 +32,10 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {
+            'posts': posts,
+            'tag':tag
+            }
     )
 
 
