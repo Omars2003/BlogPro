3bb9ed4785e3c55114ed584c9a49b91883a787d6
Omar <omartinezgarcia670@gmail.com>
2024-08-29 16:04:57 +0000

    BLOG FINAL

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -87,14 +87,15 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-
 }
+
+
 
 
 # Password validation
